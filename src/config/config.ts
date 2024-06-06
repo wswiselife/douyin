63709ae7ff790dbd90da8229ba576719759fb70c
wswiselife <wswiselife@gmail.com>
2024-06-06 08:55:30 +0800

    后端地址https配置

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
-const BASE_URL = "http://110.41.17.28:3000";
-// const BASE_URL = 'http://localhost:3000'
+const BASE_URL = "https://110.41.17.28:3443";
+// const BASE_URL = "https://localhost:443";
+// const BASE_URL = "http://localhost:3000";
 
 export default {
     BASE_URL,
