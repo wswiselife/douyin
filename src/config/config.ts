467a3abbd165710b6bbaf054793053a430240f2a
wswiselife <wswiselife@gmail.com>
2024-06-14 09:37:27 +0800

    20240614

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 // const BASE_URL = "https://110.41.17.28:3443";
 // 线上http--2024-06-06
 // const BASE_URL = "http://110.41.17.28:3080"; // 暂时不做处理，视频路径错误问题
-const BASE_URL = "http://110.41.17.28:3000";
+// const BASE_URL = "http://110.41.17.28:3000";
 // const BASE_URL = "https://localhost:443";
-// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:3000";
 
 export default {
     BASE_URL,
